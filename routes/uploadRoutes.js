743fe0a4618da147c8d64bc022471bc4a95cb05b
CharlesOkechukwu <realcharlieok@gmail>
2025-10-17 15:47:53 +0100

    add upload middleware

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { uploadImageController, deleteImageController } = require('../controllers/uploadController');
+const { upload } = require('../middleware/uploadMiddleware');
 
 const router = express.Router();
 
 // Route to handle image upload
-router.post('/upload', uploadImageController);
+router.post('/upload', upload.single('image'), uploadImageController);
 
 // Route to handle image deletion
-router.post('/delete', deleteImageController);
+router.delete('/delete', deleteImageController);
 
 module.exports = router;
