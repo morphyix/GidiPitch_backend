e0a008ef52843112fa9d67f684f9c1b65c8d292b
CharlesOkechukwu <realcharlieok@gmail>
2025-11-08 17:18:14 +0100

    fix password reset bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -262,7 +262,7 @@ const userForgotPassword = async (req, res, next) => {
             // create a reset password token
             const resetToken = createJwtToken({ email: user.email, id: user._id }, '5m');
             // create reset password url
-            const resetUrl = `${process.env.FRONTEND_URL}/auth/password/reset?token=${resetToken}`;
+            const resetUrl = `${process.env.LIVE_URL}/auth/password/reset?token=${resetToken}`;
 
             const resetPasswordEmailData = {
                 to: user.email,
